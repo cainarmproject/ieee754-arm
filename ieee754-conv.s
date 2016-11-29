1d8590d94ba1f3705f7ba989b8a8399eefb29638
Nisarga Patel <np8@umbc.edu>
2016-11-29 13:18:41 -0500

    Added code that calculates both power of 10 and number of iterations based on number of digits in first decimal part

--- ieee754-conv.s
+++ ieee754-conv.s
@@ -5,23 +5,41 @@
 .text
 .global _start
 	
-	LDR r0, =0x5c035ba0 ; load literal like this example 23555.23456
+;	LDR r0, =0x5c035ba0 ;load literal like this example 23555.23456
+	LDR r0, =0x00060006
 	LDR r2, =0x0000ffff ; set the bits we want
-	LDR r3, =0x00000000
+	LDR r3, =0xffff0000
 	AND r2, r0, r2
+	AND r3, r0, r3
+	MOV r3, r3, ROR#16
 	MOV r11, #0
+	MOV r7, #1
+	LDR r4, =0b01
+loop_pow_two:
+	MOV r4, r4, LSL#1
+	CMP r4, r3
+	ADDLS r7, r7, #1
+	BLS loop_pow_two
+
+; now we have the number of binary digits in the first part
+; for example 0x000a is 0b1010 which is 4 digits is stored in r7
+; now we can do 23 - r7 to get the number of loop iterations for last part
 loop_pow_ten:
 	LDR r10, =pows_ten
 	LDR r9, [r10, r11]
 	ADD r11, r11, #4	
 	CMP r9, r2
-	BLT loop_pow_ten
-; at this point the power of 10 that is greater than decimal part is in r9
-next:
-	; now we can do the shift left by 1 loop and compare against r9
+	BLS loop_pow_ten
+
+    ; now we have the power of 10 to compare against stored in r9
+	LDR r8, =0x18 ; 24 in hex is 0x18, 24 not 23 because # of digits - 1
+	SUB r11, r8, r7
+	LDR r8, =0x00000000
+	; r11 contains number of next loop iterations
+finish:
 	SWI 0x11		
 .data
-	pows_ten: .word 0x1, 0xA, 0x64, 0x3E8, 0x2710, 0x186A0
+	pows_ten: .word 0x1,  0xA, 0x64, 0x3E8, 0x2710, 0x186A0
 
 
 
